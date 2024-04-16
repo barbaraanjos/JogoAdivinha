004e0b88ec4f0f398fb5a5fcb4be1a153880588a
barbara.anjos <barbara.anjos@segfy.com>
2024-04-15 21:52:52 -0300

    Add project files.

---
+++ JogoAdivinha/Program.cs
@@ -0,0 +1,25 @@
+﻿//Definindo um numero aleatorio
+Random numeroAleatorio = new Random();
+int numeroSecreto = numeroAleatorio.Next(1, 101);
+
+do 
+{
+	Console.WriteLine("Digite um número entre 1 e 100: ");
+	int chute = int.Parse(Console.ReadLine());
+
+	if (chute == numeroSecreto)
+	{
+		Console.WriteLine("Parabéns! Você acertou o número.");
+		break;
+	}
+	else if (chute < numeroSecreto)
+	{
+		Console.WriteLine("O número secreto é maior que o número digitado.");
+	}
+	else
+	{
+		Console.WriteLine("O número secreto é menor que o número digitado.");
+	}
+} while (true);
+
+Console.WriteLine("Fim do jogo! Você acertou o número secreto.");
